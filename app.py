11144470a0f62fa93bd537a14877f7eb39ec0ded
NexusProLab <prolab01@proton.me>
2025-12-27 21:41:26 -0500

    Test new version deployed via CI/CD

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return f"Hello! I am running inside container: {socket.gethostname()}"
+    return f"Hello from GITHUB ACTIONS! This is a NEW VERSION running in container: {socket.gethostname()}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
